d92f38c4935b25c342918798612abf145295f88f
retz8 <ekkicb71@gmail.com>
2023-08-10 23:49:51 +0900

    new env test

--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -3,6 +3,7 @@ import ClubLogoText from "./ClubLogoText";
 import Navigation from "./Navigation";
 
 export default function NavBar() {
+  // test
   return (
     // px는 padding x방향, py는 padding y방향
     <div className="w-full flex items-center mx-auto justify-between px-4 py-6">
